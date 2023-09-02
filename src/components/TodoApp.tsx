a302f3cea0b784442d2e86bf8c46683a92bf12b0
nashdev255 <yutori1107@gmail.com>
2023-09-03 06:00:49 +0900

    TodoApp.tsx

---
+++ src/components/TodoApp.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TodoApp = () => {
+    return (
+        <section className="text-center mb-2 text-2xl font-medium">
+            <h3>Supabase Todo App</h3>
+            <form>
+                <input type="text" className='shadow-lg'/>
+            </form>
+        </section>
+    )
+};
+
+export default TodoApp;
